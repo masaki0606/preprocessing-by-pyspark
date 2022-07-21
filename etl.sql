85cf45207dc40265c4ea4aa48d0cd9bc37357b1c
isomasaki <masaki.iso@rakus-partners.co.jp>
2022-07-21 09:21:03 +0900

    設定ファイルからの読み込み

---
+++ etl.sql
@@ -0,0 +1,6 @@
+Insert overwrite table jinko_avg PARTITION(kenmei)
+select avg(jinko_male) as male_avg,avg(jinko_female),kenmei as female_avg 
+ from jinko
+  where gengo='{GENGO}' and kenmei != '人口集中地区以外の地区'
+ group by kenmei
+ order by male_avg
